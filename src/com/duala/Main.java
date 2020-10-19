b30e52f7f50b7bebc7bc6c31fc996d204c5f6c71
Kweku <kwekuappiah11@gmail.com>
2020-10-19 18:28:02 +0000

    Created basic structure

--- src/com/duala/Main.java
+++ src/com/duala/Main.java
@@ -3,6 +3,12 @@ package com.duala;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int n = 4;
+        while (n==4){
+
+            if (n==4)
+                continue;
+            System.out.println(n);
+        }
     }
 }
